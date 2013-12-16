b46332e6f0a8c6f28e472faba541e352def4de29
slaFFik <slaFFik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-16 15:16:42 +0000

    BuddyPress FOL - v0.4.3.1
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-friends-on-line/tags/0.4.3.1@823211 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bp-friends-on-line.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Plugin Name: BuddyPress Friends On-line (FOL)
+Plugin URI: http://wp-bp.com/products#friends_article
+Description: Plugin will display on your Friends page a new tab called Online with a list of currently online friends. <a href="http://wp-bp.com">Powered by WP-BP.com</a>
+Author: WP-BP
+Version: 0.4.3.1
+Author URI: http://wp-bp.com/
+*/
+
+add_action('bp_init', 'fol_menu');
+function fol_menu(){
+    global $bp;
+
+    load_plugin_textdomain( 'fol', false, dirname( plugin_basename( __FILE__ ) ) . '/langs/' );
+
+    $friends_link = trailingslashit( $bp->loggedin_user->domain . bp_get_friends_slug() );
+
+    $sub_nav = array(
+        'name'            => __( 'Online', 'fol' ),
+        'slug'            => 'online',
+        'parent_url'      => $friends_link,
+        'parent_slug'     => bp_get_friends_slug(),
+        'screen_function' => 'friends_screen_my_friends',
+        'position'        => 15,
+        'item_css_id'     => 'friends-online',
+        'user_has_access' => bp_is_my_profile()
+    );
+    bp_core_new_subnav_item($sub_nav);
+}
+
+// get the online friends list
+add_filter('bp_core_get_users', 'fol_get_online', 10, 2);
+function fol_get_online($data, $params){
+    extract( $params, EXTR_SKIP );
+    if(bp_is_user_friends() && bp_is_current_action( 'online') ){
+        $type    = 'online';
+        $user_id = bp_loggedin_user_id();
+    }
+    return BP_Core_User::get_users( $type, $per_page, $page, $user_id, $include, $search_terms, $populate_extras, $exclude, $meta_key, $meta_value );
+}
+
+if ( file_exists( dirname(__File__) . '/bpfol-custom-widget.php' ) )
+	include (dirname(__File__) . "/bpfol-custom-widget.php");
+?>
---
+++ bpfol-widget-template.php
@@ -0,0 +1,32 @@
+<?php if($instance['view_mode'] == 'standart'):?>
+    <ul id="members-list" class="item-list" role="main">
+    <?php while ( bp_members() ) : bp_the_member(); ?>
+        <li>
+            <?php if($instance['display_avatar']):?>
+                <div class="item-avatar">
+                    <a href="<?php bp_member_permalink(); ?>"><?php bp_member_avatar(); ?></a>
+                </div>
+            <?php endif;?>
+            <div class="item">
+                <div class="item-title">
+                <?php if($instance['display_name']):?>
+                    <a href="<?php bp_member_permalink(); ?>"><?php bp_member_name(); ?></a>
+                <?php endif;?>
+                </div>
+                <?php if($instance['display_last_activity']):?>
+                    <div class="item-meta"><span class="activity"><?php bp_member_last_active(); ?></span></div>
+                <?php endif;?>
+            </div>
+            <div class="clear"></div>
+        </li>
+    <?php endwhile; ?>
+    </ul>
+<?php elseif($instance['view_mode'] == 'only_avatar'):?>
+    <div class="avatar-block">
+        <?php while ( bp_members() ) : bp_the_member(); ?>
+            <div class="item-avatar">
+                <a href="<?php bp_member_permalink() ?>" title="<?php bp_member_name() ?>"><?php bp_member_avatar() ?></a>
+            </div>
+        <?php endwhile; ?>
+    </div>
+<?php endif;?>
